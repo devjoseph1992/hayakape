400d687a023834327262a72f57607ef065436630
devjoseph1992 <devjosephflorina@gmail.com>
2023-12-15 15:57:58 +0800

    adding categories section and filters provider

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -20,7 +20,7 @@ import { generateMeta } from '../../_utilities/generateMeta'
 // If you are not using Payload Cloud then this line can be removed, see `../../../README.md#cache`
 export const dynamic = 'force-dynamic'
 
-// import Categories from '../../_components/Categories'
+import Categories from '../../_components/Categories'
 // import Promotion from '../../_components/Promotion'
 
 import classes from './index.module.scss'
@@ -66,7 +66,7 @@ export default async function Page({ params: { slug = 'home' } }) {
           <Hero {...hero} />
 
           <Gutter className={classes.home}>
-            {/* <Categories categories={categories} /> */}
+            <Categories categories={categories} />
             {/* <Promotion /> */}
           </Gutter>
         </section>
